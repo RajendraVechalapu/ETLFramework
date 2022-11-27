0ecfb8391e9d858a74b58b85afbd566ae87fbe6c
Rajendra Vechalapu <vrsanaidu@gmail.com>
2022-11-27 18:58:15 +1300

    Change latest

--- ETLFramework/ETLFramework/ETL/Tables/SourceTablesDataLoadDetails.sql
+++ ETLFramework/ETLFramework/ETL/Tables/SourceTablesDataLoadDetails.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [ETL].[SourceTablesDataLoadDetails] (
     [ID]                     INT           IDENTITY (1, 1) NOT NULL,
     [SourceServer]           VARCHAR (250) NOT NULL,
+    [SourceDatabase]         VARCHAR (MAX) NULL,
     [SourceTable]            VARCHAR (150) NOT NULL,
-    [SourceTableColumnNames] VARCHAR (MAX) NOT NULL,
+    [SourceQuery]            VARCHAR (MAX) NOT NULL,
+    [SourceTableColumnNames] VARCHAR (MAX) NULL,
     [LandingTargetTable]     VARCHAR (150) NOT NULL,
     [TargetTable]            VARCHAR (150) NOT NULL,
     [DataLoadType]           VARCHAR (150) NOT NULL,
@@ -11,6 +13,8 @@
     [KeyColumns]             VARCHAR (150) NULL,
     CONSTRAINT [PK_TablesObjectsCreation] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 
---
+++ ETLFramework/ETLFramework/dbo/Tables/TableLoadReporttest.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TableLoadReporttest] (
+    [SourceTableName] NVARCHAR (500) NULL,
+    [TargetTableName] NVARCHAR (500) NULL,
+    [RowCount]        INT            NULL,
+    [RowsInserted]    INT            NULL,
+    [RowsUpated]      INT            NULL,
+    [RowsDeleted]     INT            NULL,
+    [AsAtDate]        DATETIME       NULL,
+    [Status]          VARCHAR (100)  NULL,
+    [Comments]        NVARCHAR (MAX) NULL
+);
+
